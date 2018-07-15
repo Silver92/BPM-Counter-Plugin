5b5ebd643ef78b727d6f0a6267d7cf560110399b
WenXiao <wen.xiao@mail.mcgill.ca>
2018-07-15 18:06:09 -0400

    Finished the basic functions' implementation. Restrict the BPM in one digit.
    To solve:
    1. no keyboard response after open the plugin. When clicking the mouse once the keyboard works.
    2. Prevent the keyboard continueing outputing responses when holding the spacekey down

--- Source/Counter.h
+++ Source/Counter.h
@@ -9,6 +9,12 @@
 */
 
 #pragma once
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+using namespace std::chrono;
 
 class BPMCounter: public Component, KeyListener {
 public:
@@ -24,29 +30,53 @@ public:
     void paint (Graphics& g) override
     {
         g.setColour (getLookAndFeel().findColour (Label::textColourId));
-        g.setFont (14.0f);
+        g.setFont (40.0f);
         g.drawFittedText (message, getLocalBounds().reduced (10, 0), Justification::centred, 4);
     }
     
     
     bool keyPressed(const KeyPress& k, Component* comp) override {
         if(k.getKeyCode() == KeyPress::spaceKey) {
-            message = "Space pressed";
+            countBeats();
         }
         repaint();
         return true;
     }
     
-    
     void mouseDown (const MouseEvent& e) override {
-        lastMousePosition = e.position;
-        message = String(e.position.x);
+        countBeats();
         repaint();
     }
     
-    String message {"This is the massage"};
+private:
+    void countBeats() {
+        timeCount.add(Time::getMillisecondCounterHiRes());
+        count++;
+        message = std::to_string(count);
+        if(count == 8) {
+            time = (timeCount.getLast()-timeCount.getFirst())/(count-1);
+            time = 60*1000.0/time;
+            time = round(time, 1);
+            message = std::to_string(time);
+            message = message.substr(0, (message.find_last_of(".")+2));
+            timeCount.clear();
+            count = 0;
+        }
+    }
+    
+    double round(double src, int bits)
+    {
+        stringstream ss;
+        ss << fixed << setprecision(bits) << src;
+        ss >> src;
+        return src;
+        
+    }
     
 private:
+    double time;
+    Array<double> timeCount;
+    int count = 0;
+    std::string message {"This is the massage"};
     
-    Point<float> lastMousePosition;
 };
